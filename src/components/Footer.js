4be1ec6c42657d46082c98f1a955c50563f72f4a
Raul <rtf8519@gmail.com>
2021-01-13 16:26:11 -0500

    all pages up and running

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+function Footer () {
+    return (
+        <footer> 
+            <p id="copyright"> &copy; Copyright</p>
+
+            <p id="socialmedia">
+            <a href="https://www.linkedin.com/in/raulfernandez-859793176 "className="fa fa-linkedin"></a>
+            <a href="http://github.com/RaulF419" className="fa fa-github"></a>
+
+            </p>
+
+        </footer>
+    )
+};
+
+export default Footer;
---
+++ src/models/projects.js
@@ -0,0 +1,44 @@
+import Burger from "../assets/Burger.png";
+import DailyPlanner from "../assets/DailyPlanner.png";
+import HP from "../assets/HarryPotterAPI.png";
+import QuarantineCoach from "../assets/QuarantineCoach.png";
+import FitnessTracker from "../assets/FitnessTracker.png" ;
+import progressiveBudget from "../assets/Budget.png";
+
+
+
+
+const Projects = [
+    {
+        name: "Burger",
+        img: Burger,
+        git: "https://github.com/RaulF419/burger.git"
+    },
+    {
+        name: "Daily Planner",
+        img: DailyPlanner,
+        git: "https://github.com/RaulF419/homeworkFive.git"
+    },
+    {
+        name: "Harry Potter API",
+        img: HP,
+        git: "https://github.com/Doseofcarlos/movie-api.2.git"
+    },
+    {
+        name: "Quarantine Coach",
+        img: QuarantineCoach,
+        git: "https://github.com/Skirkp18/Group_10_Project_2.git"
+    },
+    {
+        name: "Fitness Tracker",
+        img: FitnessTracker,
+        git: "https://github.com/RaulF419/workoutTracker.git"
+    },
+    {
+        name: "Budget",
+        img: progressiveBudget,
+        git: "https://github.com/RaulF419/progressiveBudget.git"
+    }
+];
+
+export default Projects;
